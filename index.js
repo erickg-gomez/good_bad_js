5cb0924492637e7f5d0b6bc95b18e2c7f038eb69
Erick Gomez <erickg.gomez@gmail.com>
2016-07-21 01:39:54 -0400

    problems resolved 6-9

--- index.js
+++ index.js
@@ -20,3 +20,35 @@ console.log(mul(3,5))
 console.log(idf(identity(3))())
 console.log(addf(3)(5))
 console.log(applyf(add)(3)(5))
+
+
+//Problems 6-9
+
+//(6) Write a fn that takes a fn and an argument, and returns a fn that can
+//supply a second argument.
+//const curry = (fn, arg1) => applyf(fn)(arg1)
+const curry = (fn, ...arg1) => (...arg2) => fn(...arg1, ...arg2)
+
+//(7) Without writing any new fn show three ways to create the inc fn.
+const inc1 = curry(add, 1)
+const inc2 = addf(1)
+const inc3 = applyf(add)(1)
+
+//(8) Write a methodize, a fn that converts a binary fn to a methodize
+const methodize = (fn) => {
+    return function(...arg) {
+        return fn(this, ...arg)
+    }
+}
+Number.prototype.add = methodize(add)
+
+//(9) Write demethodize, a fn that convert a method to a binary fn
+const demethodize = (fn) => (that, ...arg) => fn.apply(that, arg)
+
+//test
+console.log(curry(mul, 3)(5))
+console.log(inc1(5))
+console.log(inc2(5))
+console.log(inc3(5))
+console.log((3).add(8))
+console.log(demethodize(Number.prototype.add)(3, 8))
